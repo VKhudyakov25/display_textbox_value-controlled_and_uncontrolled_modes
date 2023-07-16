9af3e86afd918513711a962088f6d7e18017597e
Vyacheslav Khudyakov <slavak521@MacBook-Air-Vyacheslav.local>
2023-07-16 17:00:53 +0600

    Task complited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "devextreme/dist/css/dx.dark.css";
+import "./App.css";
+import Button from "devextreme-react/button";
+import TextBox from "devextreme-react/text-box";
+import { useCallback, useState, useRef } from "react";
+
+let textValue = "";
 
 function App() {
+  const [content, setContent] = useState(false);
+  const [value, setValue] = useState("");
+  const inputRef = useRef(null);
+
+  const handleValueChanged = useCallback((v) => {
+    setValue(v.value);
+  }, []);
+
+  const onClick = () => {
+    textValue = value;
+    setContent(!content);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button text="Click me!" type="success" onClick={onClick} />
+      <TextBox
+        className="textbox"
+        value={value}
+        onValueChanged={handleValueChanged}
+        label="Link"
+        labelMode="floating"
+      />
+      <TextBox
+        className="textbox"
+        ref={inputRef}
+        defaultValue=""
+        label="Link"
+        labelMode="floating"
+      />
+      {content && <TextBoxesContent inputRef={inputRef} value={textValue} />}
+    </div>
+  );
+}
+
+function TextBoxesContent(props) {
+  const { value, inputRef } = props;
+  return (
+    <div className="text-boxes-value">
+      <p>{value}</p>
+      <p>{inputRef.current.instance.option("value")}</p>
     </div>
   );
 }
